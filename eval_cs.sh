b5786b0e38d3720f1794ac9f9cc9839c4e043102
someshs <someshs@adobe.com>
2024-01-31 15:45:00 +0530

    Fixed CS BS scripts

--- eval_cs.sh
+++ eval_cs.sh
@@ -5,19 +5,19 @@ gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
 IFS=',' read -ra GPULIST <<< "$gpu_list"
 
 CHUNKS=${#GPULIST[@]}
-CKPT="henry-vid-cs-bs-0/checkpoint-750/"
+CKPT = "henry-vid-cs-bs-0"
 OPENAIKEY=""
 OPENAIBASE=""
 
 for IDX in $(seq 0 $((CHUNKS-1))); do
-    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python llamavid/eval/model_activitynet_qa.py \
+    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python llamavid/eval/model_lambda_qs.py \
     --model-path ./work_dirs/$CKPT \
-    --video_dir ./data/ \
-    --gt_file_question ./data/test_q.json \
-    --gt_file_answers ./data/test_a.json \
+    --gt_file_question ./data/cs_test_q.json \
+    --gt_file_answers ./data/cs_test_a.json \
     --output_dir ./work_dirs/eval-$CKPT \
     --output_name pred \
     --num-chunks $CHUNKS \
     --chunk-idx $IDX \
     --conv-mode vicuna_v1 &
 done
+wait
