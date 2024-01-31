7c7487e9ae75711fd1791d6d9d3ea89fe362c30e
someshs <someshs@adobe.com>
2024-01-31 12:13:53 +0530

    Add eval script

--- eval.sh
+++ eval.sh
@@ -5,21 +5,24 @@ gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
 IFS=',' read -ra GPULIST <<< "$gpu_list"
 
 CHUNKS=${#GPULIST[@]}
-
-CKPT="henry-vid/checkpoint-300/"
+CKPTS="henry-vid-cs-bs-0/checkpoint-200/ henry-vid-cs-bs-0/checkpoint-250/ henry-vid-cs-bs-0/checkpoint-300/ henry-vid-cs-bs-0/checkpoint-350/ henry-vid-cs-bs-0"
 OPENAIKEY=""
 OPENAIBASE=""
 
-for IDX in $(seq 0 $((CHUNKS-1))); do
-    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python llamavid/eval/model_activitynet_qa.py \
-    --model-path ./work_dirs/$CKPT \
-    --video_dir ./data/videos \
-    --gt_file_question ./data/test_q.json \
-    --gt_file_answers ./data/test_a.json \
-    --output_dir ./work_dirs/eval-$CKPT \
-    --output_name pred \
-    --num-chunks $CHUNKS \
-    --chunk-idx $IDX \
-    --conv-mode vicuna_v1 &
-
+for CKPT in $CKPTS; do
+    for IDX in $(seq 0 $((CHUNKS-1))); do
+        CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python llamavid/eval/model_activitynet_qa.py \
+        --model-path ./work_dirs/$CKPT \
+        --video_dir ./data \
+        --gt_file_question ./data/test_q.json \
+        --gt_file_answers ./data/test_a.json \
+        --output_dir ./work_dirs/eval-$CKPT \
+        --output_name pred \
+        --num-chunks $CHUNKS \
+        --chunk-idx $IDX \
+        --conv-mode vicuna_v1 &
+    done
+    wait
+    cat ./work_dirs/eval-$CKPT/*_*.json > ./work_dirs/eval-$CKPT/pred.json
+    rm ./work_dirs/eval-$CKPT/*_*.json
 done
